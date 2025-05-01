c56727fca1f08b0bd12c3448390435f53b686c3b
Poperie <thelegendpvp100@gmail.com>
2025-05-01 10:54:09 +0200

    Update LogInButton.js

--- components/navigation/LogInButton.js
+++ components/navigation/LogInButton.js
@@ -3,17 +3,17 @@
 import { createClient } from "@/utils/supabase/client";
 
 export default function LogInButton() {
-  return (
-    <button
-      onClick={() => {
-        createClient().auth.signInWithOAuth({
-          provider: "google",
-          options: { redirectTo: `http://localhost:3000/auth/callback` },
-        });
-      }}
-      className="btn"
-    >
-      Log ind
-    </button>
-  );
+    return (
+        <button
+            onClick={() => {
+                createClient().auth.signInWithOAuth({
+                    provider: "google",
+                    options: { redirectTo: `https://www.kildekritisk.dk/auth/callback` },
+                });
+            }}
+            className="btn"
+        >
+            Log ind
+        </button>
+    );
 }
