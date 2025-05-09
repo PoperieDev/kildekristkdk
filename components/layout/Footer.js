7c7802333f3d0a351c1361f055b46d285c3f48d4
Poperie <thelegendpvp100@gmail.com>
2025-05-09 15:04:20 +0200

    Update Footer.js

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,17 +1,17 @@
 export default function Footer() {
-  return (
-    <footer className="footer grid place-items-center sm:footer-horizontal bg-neutral text-neutral-content relative">
-      <div className="w-full h-full absolute bg-neutral blur-xl -z-10"></div>
-      <div className="container footer sm:footer-horizontal p-10">
-        <nav>
-          <h6 className="footer-title">Info</h6>
-          <a className="">Udviklet på HTX</a>
-          <a className="">SO-Opgave projekt</a>
-          <a className="">07/05/2025</a>
-        </nav>
-        <nav></nav>
-        <nav></nav>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="footer grid place-items-center sm:footer-horizontal bg-neutral text-neutral-content relative">
+            <div className="w-full h-full absolute bg-neutral blur-xl -z-10"></div>
+            <div className="container footer sm:footer-horizontal p-10">
+                <nav>
+                    <h6 className="footer-title">Info</h6>
+                    <a className="">Udviklet på HTX</a>
+                    <a className="">Afsluttende projekt</a>
+                    <a className="">07/05/2025</a>
+                </nav>
+                <nav></nav>
+                <nav></nav>
+            </div>
+        </footer>
+    );
 }
